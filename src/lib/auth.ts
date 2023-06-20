5d6a0a2dd622bb534e2b6593c54239842b48958f
Sai Sawant <saisawant2003@gmail.com>
2023-06-21 01:35:20 +0530

    added OAuth clinet

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,53 @@
 import { NextAuthOptions } from "next-auth";
 import { db } from "./db";
-import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
-export const authOptions:NextAuthOptions = {
- adapter:PrismaAdapter(db),
- session:{
-   strategy:"jwt",
- },
- pages:{
-   signIn:"/sign-in",
- },
- providers:[
+import {nanoid} from 'nanoid'
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
     GoogleProvider({
-      clientId:process.env.GOOGLE_CLIENT_ID!,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET!
-    })
- ]
-
-}
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        (session.user.id = token.sub),
+          (session.user?.name = token.name),
+          (session.user?.image = token.picture),
+          (session.user?.username = token.username);
+      }
+      return session;
+    },
+    async jwt(token,user) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email:token.email
+        },
+      })
+      if(!dbUser){
+        token.id = user!.id;
+        return token
+      }
+      if(dbUser.username){
+        await db.user.update({
+          where:{
+            id:dbUser.id
+          },
+          data:{
+            username:nanoid(10)
+          }
+        })
+      }
+    }
+  },
+  
+};
