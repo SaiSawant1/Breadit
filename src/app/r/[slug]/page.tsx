d5587dc8123de7c5c96f377a4812d0841e290e78
Sai Sawant <saisawant2003@gmail.com>
2023-06-28 20:28:37 +0530

    completed the feed

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import MiniCreatePost from "@/components/MiniCreatePost";
+import PostFeed from "@/components/PostFeed";
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -34,6 +35,7 @@ const page = async ({ params }: PageProps) => {
       <h1 className="font-bold text-3xl md:4xl h-14 ">r/{subreddit.name}</h1>
       <MiniCreatePost session={session} />
       {/* TODO: Show posts in user feed */}
+      <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
     </>
   );
 };
