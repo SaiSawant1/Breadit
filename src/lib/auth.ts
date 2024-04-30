b239d18244ec94e7d988c53ae79e9bf9ec463d32
SaiSawant1 <saisawant2003@gmail.com>
2024-04-30 19:21:31 +0530

    bug fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,34 +1,34 @@
-import { db } from '@/lib/db'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { nanoid } from 'nanoid'
-import { NextAuthOptions, getServerSession } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import { db } from "@/lib/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { nanoid } from "nanoid";
+import { NextAuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   pages: {
-    signIn: '/sign-in',
+    signIn: "/sign-in",
   },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
     async session({ token, session }) {
       if (token) {
-        session.user.id = token.id
-        session.user.name = token.name
-        session.user.email = token.email
-        session.user.image = token.picture
-        session.user.username = token.username
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.username = token.username;
       }
 
-      return session
+      return session;
     },
 
     async jwt({ token, user }) {
@@ -36,11 +36,11 @@ export const authOptions: NextAuthOptions = {
         where: {
           email: token.email,
         },
-      })
+      });
 
       if (!dbUser) {
-        token.id = user!.id
-        return token
+        token.id = user!.id;
+        return token;
       }
 
       if (!dbUser.username) {
@@ -51,7 +51,7 @@ export const authOptions: NextAuthOptions = {
           data: {
             username: nanoid(10),
           },
-        })
+        });
       }
 
       return {
@@ -60,12 +60,12 @@ export const authOptions: NextAuthOptions = {
         email: dbUser.email,
         picture: dbUser.image,
         username: dbUser.username,
-      }
+      };
     },
     redirect() {
-      return '/'
+      return "/";
     },
   },
-}
-export const getAuthSession = ()=> getServerSession(authOptions) 
+};
+export const getAuthSession = () => getServerSession(authOptions);
 /* {"id":"0vxbo8jrqzlm","type":"CurrentUser","display_name":"Sai Suhas Sawant","name":"Sai Suhas Sawant","email":"saisawant2003@gmail.com","unconfirmed_email":null,"avatar_url":"https://app.planetscale.com/gravatar-fallback.png","created_at":"2023-06-21T14:03:39.579Z","updated_at":"2023-06-21T14:03:51.522Z","flags":{"data_imports":"full","deployment_revert_beta":"full","insights_tablet_type":"full"},"two_factor_auth_required":false,"two_factor_auth_configured":false,"session_expires_at":null,"session_refresh_url":null,"email_verified":false,"staff":false,"oauth":true,"sso":false,"managed":true,"directory_managed":false,"authenticated_cli":false,"tos":true,"read_only":false,"has_oauth_tokens":false} */
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,10 @@
-import {Redis} from "@upstash/redis"
+import { Redis } from "@upstash/redis";
 
-export const redis=new Redis({
-    url:process.env.REDIS_URL!,
-    token:process.env.REDIS_SECRET!,
-})
+export const revalidate = 0; // disable cache
+
+export const redis = new Redis({
+  url: "https://apn1-tolerant-gull-33989.upstash.io",
+  token:
+    "AYTFASQgZTZhNWViZWEtYTMwYS00YzZhLWFiZDYtYWIwMDA5NzIzZmU0Yjc1ZWY0NTY0YmUyNDQ3Yjk0MTI4ZmIwZTE0Y2UwNjQ=",
+});
+
